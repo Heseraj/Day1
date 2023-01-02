c7113f2168f3ad0ed3a7f4d4156794255051df6f
HeSer <B@nd@r1359>
2023-01-02 11:02:44 -0500

    udated v1

--- Day1.py
+++ Day1.py
@@ -1,3 +1,16 @@
 # This is day one coding challenge
 print("Hello Mosa!")
 print("This is also a new Start")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this: ")
+print("Print('What to Print')")
+
+# It never hurts to refresh memory
+
+print("Hello Mosa. \n We can still work on your project. \n You just need to survive the hard time and keep staying focused. \n "
+      "You've areadly proven that you are capable of doing a lot of things through your hard work and dedication. \n")
+
+# Hopefully, this time I will do my best to once and for all learn python.
+
+print("New lines can be created with a backslash and n. \n")
