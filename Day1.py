cdcd1213fcb8424eda552535e41659c5b43bd89a
HeSer <B@nd@r1359>
2023-01-02 11:41:39 -0500

    update v2

--- Day1.py
+++ Day1.py
@@ -2,15 +2,24 @@
 print("Hello Mosa!")
 print("This is also a new Start")
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this: ")
-print("Print('What to Print')")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this: ")
+# print("Print('What to Print')")
+#
+# # It never hurts to refresh memory
+#
+# print("Hello Mosa. \n We can still work on your project. \n You just need to survive the hard time and keep staying focused. \n "
+#       "You've areadly proven that you are capable of doing a lot of things through your hard work and dedication. \n")
+#
+# # Hopefully, this time I will do my best to once and for all learn python.
+#
+# print("New lines can be created with a backslash and n. \n")
 
-# It never hurts to refresh memory
+# input("What is your name? ")
 
-print("Hello Mosa. \n We can still work on your project. \n You just need to survive the hard time and keep staying focused. \n "
-      "You've areadly proven that you are capable of doing a lot of things through your hard work and dedication. \n")
+# print("Hello", input("What is your name? "))
 
-# Hopefully, this time I will do my best to once and for all learn python.
+name = input("What is your name? ")
+print(len(name))
 
-print("New lines can be created with a backslash and n. \n")
+
